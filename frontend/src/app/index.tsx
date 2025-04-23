924ea23d67aceac99c6c0e8be9b3d8b580aaf232
risenguyen <risenguyen01@gmail.com>
2025-04-23 13:49:40 -0500

    Initial commit

---
+++ frontend/src/app/index.tsx
@@ -0,0 +1,31 @@
+import { routeTree } from "@/routeTree.gen";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient: queryClient, // Inject queryClient
+  },
+});
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TanStackRouterDevtools router={router} />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
---
+++ frontend/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    TanStackRouterVite({
+      target: "react",
+      autoCodeSplitting: true,
+      routesDirectory: "./src/app/routes",
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
