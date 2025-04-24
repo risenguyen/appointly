0baa864a7a5062698952f2bed7aa2891f0d44f0d
risenguyen <risenguyen01@gmail.com>
2025-04-23 22:10:12 -0500

    Update TreatmentService.cs

--- backend/src/appointly.BLL/Services/TreatmentService.cs
+++ backend/src/appointly.BLL/Services/TreatmentService.cs
@@ -1,19 +1,32 @@
 using appointly.BLL.DTOs.Treatments;
 using appointly.BLL.Exceptions;
 using appointly.BLL.Services.IServices;
+using appointly.BLL.Validators.Treatments;
 using appointly.DAL.Entities;
 using appointly.DAL.Repositories.IRepositories;
+using FluentValidation;
 
 namespace appointly.BLL.Services;
 
-public class TreatmentService(ITreatmentRepository treatmentRepository) : ITreatmentService
+public class TreatmentService(
+    ITreatmentRepository treatmentRepository,
+    CreateTreatmentRequestValidator validator
+) : ITreatmentService
 {
     private readonly ITreatmentRepository _treatmentRepository = treatmentRepository;
+    private readonly CreateTreatmentRequestValidator _validator = validator;
 
     public async Task<TreatmentResponse> CreateTreatmentAsync(
         CreateTreatmentRequest createTreatmentRequest
     )
     {
+        var validationResult = await _validator.ValidateAsync(createTreatmentRequest);
+
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
+
         var treatment = new Treatment()
         {
             Name = createTreatmentRequest.Name,
