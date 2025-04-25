a0565e692f46174c9ff968bd2c7d26f501bacd33
risenguyen <risenguyen01@gmail.com>
2025-04-24 21:41:06 -0500

    Update TreatmentsController.cs

--- backend/src/appointly.API/Controllers/TreatmentsController.cs
+++ backend/src/appointly.API/Controllers/TreatmentsController.cs
@@ -27,7 +27,7 @@ public class TreatmentsController(ITreatmentService treatmentService) : Controll
 
     [HttpGet("{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetTreatmentById(int id, CancellationToken cancellationToken)
     {
         var response = await _treatmentService.GetTreatmentByIdAsync(id, cancellationToken);
