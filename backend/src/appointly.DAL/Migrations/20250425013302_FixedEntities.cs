5d99b55f988151511a19c03abe05ae464f9f45c5
risenguyen <risenguyen01@gmail.com>
2025-04-24 20:37:23 -0500

    migration for updated entities

---
+++ backend/src/appointly.DAL/Migrations/20250425013302_FixedEntities.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace appointly.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SalonServiceId",
+                table: "EmployeeTreatments");
+
+            migrationBuilder.DropColumn(
+                name: "SalonServiceId",
+                table: "Appointments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SalonServiceId",
+                table: "EmployeeTreatments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SalonServiceId",
+                table: "Appointments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
