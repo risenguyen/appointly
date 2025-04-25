b9f7799ae882bdcdd3c03fd7471bd502206aeeb7
risenguyen <risenguyen01@gmail.com>
2025-04-25 00:48:27 -0500

    Update TreatmentsController.cs

--- backend/src/appointly.API/Controllers/TreatmentsController.cs
+++ backend/src/appointly.API/Controllers/TreatmentsController.cs
@@ -17,9 +17,9 @@ public class TreatmentsController(
     private readonly CreateTreatmentRequestValidator _validator = validator;
 
     [HttpPost]
-    [ProducesResponseType(typeof(TreatmentResponse), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> CreateTreatment(
+    public async Task<ActionResult<TreatmentResponse>> CreateTreatment(
         [FromBody] CreateTreatmentRequest createTreatmentRequest,
         CancellationToken cancellationToken
     )
@@ -33,9 +33,12 @@ public class TreatmentsController(
     }
 
     [HttpGet("{id}")]
-    [ProducesResponseType(typeof(TreatmentResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> GetTreatmentById(int id, CancellationToken cancellationToken)
+    public async Task<ActionResult<TreatmentResponse>> GetTreatmentById(
+        int id,
+        CancellationToken cancellationToken
+    )
     {
         var response = await _treatmentService.GetTreatmentByIdAsync(id, cancellationToken);
         return Ok(response);
