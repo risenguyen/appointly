3333887f9d49637dc14057607858cd9a559679f5
risenguyen <risenguyen01@gmail.com>
2025-04-26 20:08:55 -0500

    Update index.tsx

--- frontend/src/app/routes/index.tsx
+++ frontend/src/app/routes/index.tsx
@@ -1,7 +1,19 @@
+import { getApiTreatmentsById } from "@/api";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
+  async loader() {
+    const { data, error, request, response } = await getApiTreatmentsById({
+      path: {
+        id: 1,
+      },
+    });
+    console.log(data);
+    console.log(error);
+    console.log(request);
+    console.log(response);
+  },
 });
 
 function RouteComponent() {
