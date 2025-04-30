6683666fb330ca369831a4f84e7ec471b25c7456
risenguyen <risenguyen01@gmail.com>
2025-04-30 18:34:42 -0500

    Create theme-context.tsx

---
+++ frontend/src/context/theme-context.tsx
@@ -0,0 +1,76 @@
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+  type ReactNode,
+} from "react";
+
+type Theme = "light" | "dark";
+
+type ThemeContextValue = {
+  theme: Theme;
+};
+
+type ThemeContextProviderProps = {
+  key: string;
+  defaultTheme: Theme;
+  children: ReactNode;
+};
+
+const ThemeContext = createContext<ThemeContextValue | undefined>(undefined);
+
+function useTheme() {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeContextProvider");
+  }
+  return context;
+}
+
+function ThemeContextProvider({
+  key,
+  defaultTheme,
+  children,
+}: ThemeContextProviderProps) {
+  const [theme, setTheme] = useState<Theme>(() => {
+    const storedTheme = localStorage.getItem(key);
+    if (storedTheme === "light" || storedTheme === "dark") {
+      return storedTheme;
+    }
+    return defaultTheme;
+  });
+
+  const toggleTheme = useCallback(
+    () =>
+      setTheme((previousTheme) =>
+        previousTheme === "light" ? "dark" : "light",
+      ),
+    [setTheme],
+  );
+
+  const value = {
+    theme,
+    toggleTheme,
+  };
+
+  useEffect(() => {
+    const root = document.documentElement;
+
+    root.setAttribute("data-theme", theme);
+    root.classList.add("disable-transitions");
+
+    const timerId = setTimeout(() => {
+      root.classList.remove("disable-transitions");
+    }, 100);
+
+    return () => clearTimeout(timerId);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+}
+
+export { ThemeContextProvider, useTheme };
