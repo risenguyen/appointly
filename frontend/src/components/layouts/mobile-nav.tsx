c7c3d8521461be687ab42f3a97d17f66582634a7
risenguyen <risenguyen01@gmail.com>
2025-05-03 10:22:58 -0500

    tweaked the mobile navigation
    made the content height smaller
    added hover effect
    de-emphasized inactive links

--- frontend/src/components/layouts/mobile-nav.tsx
+++ frontend/src/components/layouts/mobile-nav.tsx
@@ -21,7 +21,7 @@ function MobileNav() {
           <TbMenu className="leading-0" size="20px" />
         </button>
       </DrawerTrigger>
-      <DrawerContent className="h-full">
+      <DrawerContent className="">
         <DrawerTitle className="sr-only">Mobile Navigation Menu</DrawerTitle>
         <DrawerDescription className="sr-only">
           Contains links to navigate the application sections.
@@ -30,35 +30,53 @@ function MobileNav() {
           <ul className="flex flex-col gap-6 py-12">
             <li className="flex">
               <DrawerClose asChild>
-                <Link className="text-2xl font-medium" to="/app">
+                <Link
+                  activeOptions={{
+                    exact: true,
+                  }}
+                  className="data-[status=active]:text-foreground text-muted-foreground hover:text-foreground text-2xl font-medium transition-colors"
+                  to="/app"
+                >
                   Dashboard
                 </Link>
               </DrawerClose>
             </li>
             <li className="flex">
               <DrawerClose asChild>
-                <Link className="text-2xl font-medium" to="/app/appointments">
+                <Link
+                  className="data-[status=active]:text-foreground text-muted-foreground hover:text-foreground text-2xl font-medium transition-colors"
+                  to="/app/appointments"
+                >
                   Appointments
                 </Link>
               </DrawerClose>
             </li>
             <li className="flex">
               <DrawerClose asChild>
-                <Link className="text-2xl font-medium" to="/app/clients">
+                <Link
+                  className="data-[status=active]:text-foreground text-muted-foreground hover:text-foreground text-2xl font-medium transition-colors"
+                  to="/app/clients"
+                >
                   Clients
                 </Link>
               </DrawerClose>
             </li>
             <li className="flex">
               <DrawerClose asChild>
-                <Link className="text-2xl font-medium" to="/app/treatments">
+                <Link
+                  className="data-[status=active]:text-foreground text-muted-foreground hover:text-foreground text-2xl font-medium transition-colors"
+                  to="/app/treatments"
+                >
                   Treatments
                 </Link>
               </DrawerClose>
             </li>
             <li className="flex">
               <DrawerClose asChild>
-                <Link className="text-2xl font-medium" to="/app/employees">
+                <Link
+                  className="data-[status=active]:text-foreground text-muted-foreground hover:text-foreground text-2xl font-medium transition-colors"
+                  to="/app/employees"
+                >
                   Employees
                 </Link>
               </DrawerClose>
