5ee4e2f7751509269a9d5bdf853ea5adc8e98418
risenguyen <risenguyen01@gmail.com>
2025-09-17 22:41:47 -0500

    Update login.tsx

--- frontend/src/routes/(auth)/login.tsx
+++ frontend/src/routes/(auth)/login.tsx
@@ -15,7 +15,7 @@ export const Route = createFileRoute("/(auth)/login")({
 
 function RouteComponent() {
   return (
-    <div className="flex h-dvh w-screen flex-col items-center justify-center">
+    <main className="flex h-dvh w-screen flex-col items-center justify-center">
       <div className="absolute top-4 left-6 text-lg font-medium">appointly</div>
       <div className="mb-auto pt-4"></div>
       <div className="w-full max-w-md px-6">
@@ -24,6 +24,6 @@ function RouteComponent() {
       <div className="text-muted-foreground mt-auto pb-3 text-sm">
         Made by Rise Nguyen
       </div>
-    </div>
+    </main>
   );
 }
