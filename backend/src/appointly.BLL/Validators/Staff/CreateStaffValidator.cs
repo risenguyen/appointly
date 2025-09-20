7ed914111ea584d256261f387372a2d49c4439ad
risenguyen <risenguyen01@gmail.com>
2025-09-19 22:41:17 -0500

    Refactor phone validation regex in CreateStaffValidator to enforce 10-digit format

--- backend/src/appointly.BLL/Validators/Staff/CreateStaffValidator.cs
+++ backend/src/appointly.BLL/Validators/Staff/CreateStaffValidator.cs
@@ -13,8 +13,6 @@ public class CreateStaffValidator : AbstractValidator<CreateStaffRequest>
 
         RuleFor(x => x.Email).EmailAddress().When(x => !string.IsNullOrEmpty(x.Email));
 
-        RuleFor(x => x.Phone)
-            .Matches(@"^\+?[1-9]\d{7,14}$") // E.164 international phone number format: +[country code][subscriber number], 8-15 digits
-            .When(x => !string.IsNullOrEmpty(x.Phone));
+        RuleFor(x => x.Phone).Matches(@"^\d{10}$").When(x => !string.IsNullOrEmpty(x.Phone));
     }
 }
